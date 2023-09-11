2129178253be6cc38a17266e1859bf0930d22d32
yashPundhir <yash.pundhir.prof@gmail.com>
2023-09-11 23:13:56 +0530

    js file added to add functionality

---
+++ script.js
@@ -0,0 +1,15 @@
+let openModalBtn = document.querySelector("#open-modal");
+
+let closeModalBtn = document.querySelector("#close-modal");
+
+let modal = document.querySelector("#modal");
+
+// openModalBtn.addEventListener("click", () => {
+// 	modal.showModal();
+// 	document.body.classList.add("scroll");
+// });
+
+// closeModalBtn.addEventListener("click", () => {
+// 	modal.close();
+// 	document.body.classList.remove("scroll");
+// });
